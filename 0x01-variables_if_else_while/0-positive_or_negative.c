fe96da1622cf17868a28a83119ff3444f2b3a626
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-16 17:48:55 -0700

    bext

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,16 +10,17 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	printf("%d ",n);
 
-	if(n>0)
+	if( n>0 )
 	{
 		printf("is positive");
 	} 
-	else if(n==0) 
+	else if( n==0 ) 
 	{
 		printf("is zero");
 	}
