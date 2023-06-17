5d40c038e4464b79289d99999d65d611c520181e
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-16 19:00:18 -0700

    bext

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,14 @@
  * Description : print a to z alphabets
  * Return: is 0 success
  */
- int main(void)
- {
-        int n  = 97;
-        while(n <= 122)
-        {
-                putchar(n);
-                n++;
+int main(void)
+{
+       	int n  = 97;
+        
+	while(n <= 122)
+	{
+		putchar(n);
+		n++;
         }
 	putchar(65);
 
