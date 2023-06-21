d7979c802ae20682d1c75461a04674ccd6c59cd6
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-20 17:54:19 -0700

    bext

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,17 @@
-#include <unistd.h>
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
 
-void print_alphabet_x10(void)
-{
-        int count;
-        char lower;
-
-        for (count = 1; count <= 10; count++)
-        {
-                for (lower = 'a'; lower <= 'z'; lower++)
-                {
-                        _putchar(lower);
-                }
-                _putchar('\n');
-        }
-}
+void _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int);
+void times_table(void);
+void print_times_table(int);
+#endif
