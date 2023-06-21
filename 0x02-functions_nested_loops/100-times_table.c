02118aa981ed477a3c9fecbf70e90d40317e2cd2
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-20 18:27:43 -0700

    bext

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,32 +1,49 @@
-#include <stdio.h>
 #include "main.h"
 /**
-*print_to_98 -  prints all natural numbers from n to 98
+*print_times_table - prints the n times table, starting with 0
 *
-*@n: starting point parameter
+*@n: parameter integer
 */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	int num;
+	int count, countIncrement, num, output, ref;
 
-	if (n > 98)
+	countIncrement = 0;
+	if (n < 0 || n > 15)
 	{
-		for (num = n; num > 98; num--)
-		{
-			printf("%d, ", num);
-		}
-	printf("%d\n", 98);
-	}
-	else if (n < 98)
-	{
-		for (num = n; num < 98; num++)
-		{
-			printf("%d, ", num);
-		}
-	printf("%d\n", 98);
 	}
 	else
 	{
-		printf("%d\n", 98);
+	for (count = 0; count <= n; count++)
+	{
+		_putchar(48);
+		for (num = 0; num < n; num++)
+		{
+			_putchar(',');
+			_putchar(32);
+			ref = count;
+			output = ref + (countIncrement * num);
+			if (output <= 9)
+			{
+				_putchar(32);
+				_putchar(32);
+				_putchar(output + 48);
+			}
+			else if (output <= 99)
+			{
+				_putchar(32);
+				_putchar((output / 10) + 48);
+				_putchar((output % 10) + 48);
+			}
+			else
+			{
+				_putchar((output / 100) + 48);
+				_putchar(((output % 100) / 10) + 48);
+				_putchar((output % 10) + 48);
+			}
+		}
+	countIncrement = countIncrement + 1;
+	_putchar('\n');
+	}
 	}
 }
