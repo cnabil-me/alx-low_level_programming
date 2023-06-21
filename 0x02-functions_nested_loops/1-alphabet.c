d14e2cf65e2a2a48c62909b87346974d92f40b25
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-20 17:45:03 -0700

    bext

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,14 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - write from z to z X 10
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void print_alphabet_x10(void)
+*print_alphabet - outputs all lowercase alphabets
+*/
+void print_alphabet(void)
 {
-	int count;
 	char lower;
-	for (count = 1; count <= 10; count++)
+
+	for (lower = 'a'; lower <= 'z'; lower++)
 	{
-		for (lower = 'a'; lower <= 'z'; lower++)
-		{
-			_putchar(lower);
-		}
-		_putchar('\n');
+		_putchar(lower);
 	}
+	_putchar('\n');
 }
