77bc9c35cbeae9fa120aaa5ba21d1f66d1156602
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-20 17:51:48 -0700

    bext

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,12 @@
 #include "main.h"
-/**
- * print_alphabet_x10 - write from z to z X 10
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void print_alphabet_x10(void)
-{
-	int count;
-	char lower;
 
-	for (count = 1; count <= 10; count++)
-	{
-		for (lower = 'a'; lower <= 'z'; lower++)
-		{
-			_putchar(lower);
-		}
-		_putchar('\n');
-	}
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,17 @@
 #include <unistd.h>
 int _putchar(char c);
 
+void print_alphabet_x10(void)
+{
+        int count;
+        char lower;
+
+        for (count = 1; count <= 10; count++)
+        {
+                for (lower = 'a'; lower <= 'z'; lower++)
+                {
+                        _putchar(lower);
+                }
+                _putchar('\n');
+        }
+}
