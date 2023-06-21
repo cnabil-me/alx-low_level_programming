8f0e848069039fb6a4259f2164ef69e07b3b53bc
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-20 17:27:25 -0700

    bext

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-  char letter[] = {95, 112, 117, 116, 99, 104, 97, 114};
-  int i;
+	char letter[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
 
-  for (i = 0; i < 8; i++)
-  {
-    _putchar(letter[i]);
-  }
-  _putchar('\n');
-  return (0);
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(letter[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
 
