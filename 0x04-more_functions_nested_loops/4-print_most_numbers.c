513fea5197cf4829d0f93b14ebf93afa314c4837
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-22 15:37:10 -0700

    bext

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - print the numbers 0 to 9
+ *
+ */
+void print_numbers(void)
+{
+	int a;
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
+
