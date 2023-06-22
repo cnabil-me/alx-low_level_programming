2eabff78ff9e72b86dee93ed8da05ab591e4b165
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-22 16:21:12 -0700

    bext

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,11 @@ void more_numbers(void)
 		for (b = 0; b < 15; b++)
 		{
 			if (b > 9)
-				_putchar(b / 10 + '0');
+			{
+
+				_putchar('1');
+			}
+
 			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
