35e1b69a8760a64d5f3a4c4d69f5ca5496ab6bca
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-22 15:46:18 -0700

    bext

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print the numbers 0 to 9
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
+
