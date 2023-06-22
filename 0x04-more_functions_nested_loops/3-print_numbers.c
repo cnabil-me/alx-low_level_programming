bd6d18008f5f2f1791ced9323fb8acb873bb855c
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-22 15:06:59 -0700

    bext

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul - multip twoo integers
+ *
+ * Return: 0 always
+ */
+int print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
+
