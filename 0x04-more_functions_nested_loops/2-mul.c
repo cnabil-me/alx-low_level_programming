97c8d3837bb6ab16864832c0cf904db12bd01835
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-22 15:01:44 -0700

    bext

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * mul - multip twoo integers
+ * @a: operanr 1
+ * @b: operanr 1
+ *
+ * Return: multip twoo integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
