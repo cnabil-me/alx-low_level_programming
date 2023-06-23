4c9fd591d8b585fdf122c7a08bad243e2adaddd4
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-22 19:41:27 -0700

    bext

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,7 +13,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 int isPrime(long int n);
-
 long int biggestFactor(long int a);
+void print_number(int n);
 #endif /* MAIN_H */
 
