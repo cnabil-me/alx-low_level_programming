77eb05e07e68149a6ba3b5c697dbc5ecde66f56a
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-22 18:29:04 -0700

    bext

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz
+ * Description: FizzBuzz game.
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", n);
+		n++;
+	}
+}
+
