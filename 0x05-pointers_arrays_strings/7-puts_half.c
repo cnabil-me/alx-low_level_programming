e6417867a6c8a8905d6effb384eb3e72b0bfd840
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-26 18:25:48 -0700

    bext

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - prints every other character of a string
+ * @s: the string to print
+ * Return: Void
+ */
+void puts_half(char *s)
+{
+	int i;
+	int len = strlen(s);
+
+	for (i = (len) / 2; i <= len; i++)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
+
