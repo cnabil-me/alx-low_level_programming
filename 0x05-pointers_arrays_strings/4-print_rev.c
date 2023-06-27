dfd04c44934eb45c500744071a8effbdb744c5c0
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-26 17:04:04 -0700

    bext

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - reverse string
+ * @s: string pointer
+ *
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len = strlen(s);
+
+	for (i = len; i > 0; i--)
+	{
+		printf("%c", *(s + i - 1));
+	}
+}
