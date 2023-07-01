d45e96b0e3a7903e53c4fae25d0a8c8ba0e48899
Mouhssine nabil <mouhssine0674@gmail.com>
2023-06-30 17:58:20 -0700

    bext

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,62 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-char *cap_string(char *str);
-char upper_char(char c);
+/**
+ * is_a_separator - check if a char is a seperator.
+ * @s: the char to test.
+ *
+ * Return: 1 if seperator is found else 0
+ */
+int is_a_separator(char s)
+{
+	char sep[] = {'\n', ' ', ',', ';', '!',
+		      '.', '?', '\"', '(', ')', '{', '}', '\t'};
+	int i = 0;
 
+	while (sep[i] != '\0')
+	{
+		if (s == sep[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 /**
- * cap_string - capitalized string
- * @str: string to be capitalized
+ * cap_string -  capitalizes words of a string.
+ * @str: sring to be capitalized.
  *
- * Return: String pointer
+ * Return: pointer to the  string.
  */
 char *cap_string(char *str)
 {
-	int i, j;
-	char separators[] = {' ', '\t', '\n', ',',
-			     ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int separator, i;
 
+	separator = 1;
+	i = 0;
 	while (str[i] != '\0')
 	{
-
-		if (i == 0)
+		if (separator == 1 && (str[i] >= 'a' && str[i] <= 'z'))
 		{
-			str[0] = upper_char(str[0]);
-			continue;
+			str[i] -= 32;
+			separator = 0;
 		}
-
-		for (j = 0; j < 13; j++)
-		{
-			if (str[i] == '\t')
-			{
-				str[i] = ' ';
-			}
-			if (str[i] == separators[j])
-			{
-				if (str[i + 1] != '\0')
-					str[i + 1] = upper_char(str[i + 1]);
-			}
-		}
+		separator = is_a_separator(str[i]);
 		i++;
 	}
 	return (str);
-}
-/**
- * upper_char - char to uppercase
- * @c: string to be capitalized
- *
- * Return: capitalized char
- */
-char upper_char(char c)
-{
-	if (c >= 'a' && c <= 'z')
-	{
-		int asciiCode;
-
-		asciiCode = (int) c;
-		asciiCode = asciiCode - 32;
-		c = (char)asciiCode;
-	}
-	return (c);
 }
 
