3db3b8f449ef2450b2dee0210d1b24f509976a35
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-03 16:12:23 -0700

    bext

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - pointer string to pointer caracter
+ * @s: source pointer.
+ * @to: target pointer.
+ * Return: void.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
