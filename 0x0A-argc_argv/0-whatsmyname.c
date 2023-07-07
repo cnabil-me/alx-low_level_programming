08d8bf6aca5661e35bfe7b8f6d008ee961e5f859
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-07 16:39:35 -0700

    bext

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-the entry point
+ * @argc: the number of arguments
+ * @argv: array of string arguments
+ * Description:program to print its path name
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
+
