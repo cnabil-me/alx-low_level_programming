d2dca6bc326e7d9cfda6d88df1cefc381fe8df21
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-07 17:35:21 -0700

    bext

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,18 +11,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-
-	printf("%d\n", a * b);
-	return (1);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
 
