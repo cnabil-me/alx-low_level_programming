364f68c90aa00e5f03e9ca5773781c586aa5a60a
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-13 16:39:27 -0700

    bext

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,10 +27,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size_of_s1 = _strlen_recursion(s1);
 	unsigned int size_of_s2 = _strlen_recursion(s2);
 	unsigned int i, j;
+	unsigned int last = 0;
+	void *p;
 
-	void *p = malloc(sizeof(char) * (size_of_s1 + size_of_s2 + 1));
+	p = malloc(sizeof(char) * (size_of_s1 + size_of_s2 + 1));
 
-	if (p == NULL)
+	if (p == 0)
 	{
 		return (NULL);
 	}
@@ -38,6 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < size_of_s1; i++)
 	{
 		*(char *)(p + i) = *(char *)(s1 + i);
+		last++;
 	}
 	if (n < size_of_s2)
 	{
@@ -47,9 +50,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; j < size_of_s2; j++)
 	{
 		*(char *)(p + size_of_s1 + j) = *(char *)(s2 + j);
+		last++;
 	}
 
-	*(char *)(p + size_of_s1 + size_of_s2) = '\0';
+	*(char *)(p + last + 1) = '\0';
 
 	return (p);
 }
