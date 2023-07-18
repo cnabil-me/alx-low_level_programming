1b0c3b164f83fe0dfd324a8ee007eb61a7b17305
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-17 17:49:45 -0700

    bext

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - a dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
