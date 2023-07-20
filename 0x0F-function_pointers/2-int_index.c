cc432a3afa5bac8e8da5c0468b00f1adc9f7894d
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-19 19:15:34 -0700

    bext

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,9 +9,10 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i = 0;
 
 	if (array && size && cmp)
-		for (size_t i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
 				return (i);
