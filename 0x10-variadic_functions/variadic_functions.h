90fe825a8c48ab9b0cc8d662373d8eb6de6c9338
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-20 16:15:04 -0700

    bext

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,13 +3,12 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- * struct token - a token structure token
+ * struct token - Struct token
  *
- * @token: token format
- * @f: associated function
+ * @token: format token
+ * @f: The function associated
  */
 typedef struct token
 {
@@ -20,20 +19,6 @@ typedef struct token
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char *const format, ...);
-int _putchar(char c);
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+void print_all(const char * const format, ...);
 
 #endif
-
