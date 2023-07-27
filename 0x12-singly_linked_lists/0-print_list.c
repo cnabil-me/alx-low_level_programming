37f0d8ae88b9823784eed4677565c7d9adfb38d8
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-27 15:43:34 -0700

    bext

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - print a sigle liste
+ * @h: Liste head
+ * Return: size_t signe liste node count
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("[%d] ", h->str ? h->len : 0);
+		printf("%s\n", h->str ? h->str : _NULL);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
