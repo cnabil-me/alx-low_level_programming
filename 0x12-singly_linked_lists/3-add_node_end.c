22ba6794415cf0901926e35d6cb92ade3f5a244e
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-27 18:29:32 -0700

    bext

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,17 +1,5 @@
 #include "lists.h"
 /**
- * _str_len - calc the length of string
- * @str: string to be calculated
- * Return: int length of string
- */
-int _str_len(char *str)
-{
-	if (!*str)
-		return (0);
-	return (1 + _str_len(str + 1));
-}
-
-/**
  * add_node_end - add node to the end of the list
  * @head:list head
  * @str: string to be dupplicated
@@ -21,15 +9,18 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *temp;
+	size_t nchar;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (!new_node->str)
-		return (NULL);
-	new_node->len = _str_len(new_node->str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new_node->len = nchar;
 	new_node->next = NULL;
 	temp = *head;
 
