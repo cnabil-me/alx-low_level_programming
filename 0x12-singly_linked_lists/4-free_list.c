7eac03ed869509b6db233a161334254b3348aa66
Mouhssine nabil <mouhssine0674@gmail.com>
2023-07-27 18:44:07 -0700

    bext

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,18 @@
 #include "lists.h"
 /**
  * free_list - free all node mem
- * @head:list head
- * Return: Void
+ * @head: list head
+ * Return: void
  */
 void free_list(list_t *head)
 {
-	if (!head->next)
-		free(head);
-	else
-		free_list(head->next); /* code */
+	if (!head)
+		return;
+
+	if (head->next)
+		free_list(head->next);
+
+	free(head);
+	head = NULL;
 }
 
