3b46e510799906dc3206151e53e70b61c1eba98a
Mouhssine nabil <mouhssine0674@gmail.com>
2023-08-03 17:20:55 -0700

    bext

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,28 +6,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (n >> 0)
 	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+
 		_putchar('0');
-		return;
-	}
-
-	int num_bits = sizeof(unsigned long int) * 8;
-	int leading_zeros = 1;
-
-	for (int i = num_bits - 1; i >= 0; i--)
-	{
-		unsigned long int mask = 1UL << i;
-
-		if ((n & mask) != 0)
-		{
-			leading_zeros = 0;
-			_putchar('1');
-		}
-		else if (!leading_zeros)
-		{
-			_putchar('0');
-		}
-	}
 }
 
